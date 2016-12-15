bf09c2cec3b879452a2540ba8f93aa278d2be755
ESTEBAN <ESTEBAN@DESKTOP-0D3U1R2>
2016-12-15 12:32:07 +0100

    Merge with the project SDL Master 2 provided by Ricard Pillosu

--- DoubleDragon3/DoubleDragon3/Module.h
+++ DoubleDragon3/DoubleDragon3/Module.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef __MODULE_H__
+#define __MODULE_H__
 
 class Application;
 
@@ -6,18 +7,54 @@ class Module
 {
 public:
 
-	Module()
+	Module(bool active = true) : active(active)
+	{}
+
+	~Module()
+	{}
+
+	bool IsEnabled() const
 	{
+		return active;
 	}
+
+	 bool Enable()
+	{
+		if(active == false)
+			return active = Start();
+
+		return true;
+	}
+
+	 bool Disable()
+	 {
+		 if(active == true)
+			 return active = !CleanUp();
+
+		 return true;
+	 }
 
 	virtual bool Init() 
 	{
 		return true; 
 	}
 
-	// TODO 3: Create a new method "Start()" that should be calling all modules just before the first frame
+	virtual bool Start()
+	{
+		return true;
+	}
+
+	virtual update_status PreUpdate()
+	{
+		return UPDATE_CONTINUE;
+	}
 
 	virtual update_status Update()
+	{
+		return UPDATE_CONTINUE;
+	}
+
+	virtual update_status PostUpdate()
 	{
 		return UPDATE_CONTINUE;
 	}
@@ -26,4 +63,9 @@ public:
 	{ 
 		return true; 
 	}
+
+private:
+	bool active = true;
 };
+
+#endif // __MODULE_H__
--- DoubleDragon3/DoubleDragon3/ModuleInput.cpp
+++ DoubleDragon3/DoubleDragon3/ModuleInput.cpp
@@ -3,12 +3,20 @@
 #include "ModuleInput.h"
 #include "SDL/include/SDL.h"
 
-ModuleInput::ModuleInput()
-{}
+#define MAX_KEYS 300
+
+ModuleInput::ModuleInput() : Module(), mouse({0, 0}), mouse_motion({0,0})
+{
+	keyboard = new KeyState[MAX_KEYS];
+	memset(keyboard, KEY_IDLE, sizeof(KeyState) * MAX_KEYS);
+	memset(mouse_buttons, KEY_IDLE, sizeof(KeyState) * NUM_MOUSE_BUTTONS);
+}
 
 // Destructor
 ModuleInput::~ModuleInput()
-{}
+{
+	RELEASE_ARRAY(keyboard);
+}
 
 // Called before render is available
 bool ModuleInput::Init()
@@ -26,16 +34,96 @@ bool ModuleInput::Init()
 	return ret;
 }
 
-// Called every draw update
-update_status ModuleInput::Update()
+// Called before the first frame
+bool ModuleInput::Start()
 {
-	SDL_PumpEvents();
+	return true;
+}
 
-	keyboard = SDL_GetKeyboardState(NULL);
+// Called each loop iteration
+update_status ModuleInput::PreUpdate()
+{
+	static SDL_Event event;
 
-	// TODO 1: Make the application properly close when ESC is pressed (do not use exit())
+	mouse_motion = {0, 0};
+	memset(windowEvents, false, WE_COUNT * sizeof(bool));
+	
+	const Uint8* keys = SDL_GetKeyboardState(NULL);
 
-	// Homework: Make the application close up when pressing “X” button of the window
+	for(int i = 0; i < MAX_KEYS; ++i)
+	{
+		if(keys[i] == 1)
+		{
+			if(keyboard[i] == KEY_IDLE)
+				keyboard[i] = KEY_DOWN;
+			else
+				keyboard[i] = KEY_REPEAT;
+		}
+		else
+		{
+			if(keyboard[i] == KEY_REPEAT || keyboard[i] == KEY_DOWN)
+				keyboard[i] = KEY_UP;
+			else
+				keyboard[i] = KEY_IDLE;
+		}
+	}
+
+	for(int i = 0; i < NUM_MOUSE_BUTTONS; ++i)
+	{
+		if(mouse_buttons[i] == KEY_DOWN)
+			mouse_buttons[i] = KEY_REPEAT;
+
+		if(mouse_buttons[i] == KEY_UP)
+			mouse_buttons[i] = KEY_IDLE;
+	}
+
+	while(SDL_PollEvent(&event) != 0)
+	{
+		switch(event.type)
+		{
+			case SDL_QUIT:
+				windowEvents[WE_QUIT] = true;
+			break;
+
+			case SDL_WINDOWEVENT:
+				switch(event.window.event)
+				{
+					//case SDL_WINDOWEVENT_LEAVE:
+					case SDL_WINDOWEVENT_HIDDEN:
+					case SDL_WINDOWEVENT_MINIMIZED:
+					case SDL_WINDOWEVENT_FOCUS_LOST:
+					windowEvents[WE_HIDE] = true;
+					break;
+
+					//case SDL_WINDOWEVENT_ENTER:
+					case SDL_WINDOWEVENT_SHOWN:
+					case SDL_WINDOWEVENT_FOCUS_GAINED:
+					case SDL_WINDOWEVENT_MAXIMIZED:
+					case SDL_WINDOWEVENT_RESTORED:
+					windowEvents[WE_SHOW] = true;
+					break;
+				}
+			break;
+
+			case SDL_MOUSEBUTTONDOWN:
+				mouse_buttons[event.button.button - 1] = KEY_DOWN;
+			break;
+
+			case SDL_MOUSEBUTTONUP:
+				mouse_buttons[event.button.button - 1] = KEY_UP;
+			break;
+
+			case SDL_MOUSEMOTION:
+				mouse_motion.x = event.motion.xrel / SCREEN_SIZE;
+				mouse_motion.y = event.motion.yrel / SCREEN_SIZE;
+				mouse.x = event.motion.x / SCREEN_SIZE;
+				mouse.y = event.motion.y / SCREEN_SIZE;
+			break;
+		}
+	}
+
+	if(GetWindowEvent(EventWindow::WE_QUIT) == true || GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		return UPDATE_STOP;
 
 	return UPDATE_CONTINUE;
 }
@@ -43,7 +131,23 @@ update_status ModuleInput::Update()
 // Called before quitting
 bool ModuleInput::CleanUp()
 {
-	LOG("Quitting SDL input event subsystem");
+	LOG("Quitting SDL event subsystem");
 	SDL_QuitSubSystem(SDL_INIT_EVENTS);
 	return true;
+}
+
+// ---------
+bool ModuleInput::GetWindowEvent(EventWindow ev) const
+{
+	return windowEvents[ev];
+}
+
+const iPoint& ModuleInput::GetMousePosition() const
+{
+	return mouse;
+}
+
+const iPoint& ModuleInput::GetMouseMotion() const
+{
+	return mouse_motion;
 }
